c50e68fe4e50a0b62d7042a32ec91bc9843ddd6f
Matteo Muscas <thatman.j@gmail.com>
2018-10-04 08:38:40 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |turns|
-    if counter = 0
-      return "X"
+    if counter = "X" || counter = "O"
       counter +=1
+      puts "#{counter}"
   end
 end
 end  
